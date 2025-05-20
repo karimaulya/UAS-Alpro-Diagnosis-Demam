10085ec72f24f6c04143626d30bc47da97a957d7
karimaulya <karimaulya13@gmail.com>
2025-05-20 20:42:10 +0700

    Add initial implementation of diagnosis and treatment functions

---
+++ diagnosisdeman.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+
+void garis() {
+    cout << "---------------------------------------------" << endl;
+}
+void tindakan1() {
+    garis();
+    cout << "TINDAKAN:" << endl;
+    cout << "Berobat ke fasilitas kesehatan tingkat pertama (FKTP) terdekat." << endl;
+}
+void tindakan2() {
+    garis();
+    cout << "TINDAKAN:" << endl;
+    cout << "Segera bawa ke Instalasi Gawat Darurat (IGD) rumah sakit terdekat." << endl;
+}
+void tindakan3() {
+    garis();
+    cout << "TINDAKAN:" << endl;
+    cout << "Segera berobat ke dokter spesialis penyakit dalam." << endl;
+}
+
+void diagnosis1(){
+    garis();
+    cout << "TANDA DAN GEJALA:" << endl;
+    cout << "1. Terdapat ruam pada kulit." << endl;
+    cout << "2. Terdapat muntah, penurunan kesadaran, dan atau nyeri kepala." << endl;
+    garis();
+    cout << "DIAGNOSIS:" << endl;
+    cout << "Toxic Shock Syndrome (TSS) atau Sindrom Syok Beracun." << endl;
+    tindakan2();
+}
+void diagnosis2(){
+    garis();
+    cout << "TANDA DAN GEJALA:" << endl;
+    cout << "1. Terdapat ruam pada kulit." << endl;
+    cout << "2. Tidak terdapat muntah, penurunan kesadaran, dan atau nyeri kepala." << endl;
+    cout << "3. Terdapat ruam berbentuk bintik atau berukuran kecil." << endl;
+    garis();
+    cout << "DIAGNOSIS:" << endl;
+    cout << "Demam Berdarah." << endl;
+    tindakan1();
+}
+void diagnosis3(){
+    garis();
+    cout << "TANDA DAN GEJALA:" << endl;
+    cout << "1. Terdapat ruam pada kulit." << endl;
+    cout << "2. Tidak terdapat muntah, penurunan kesadaran, dan atau nyeri kepala." << endl;
+    cout << "3. Tidak terdapat ruam berbentuk bintik atau berukuran kecil." << endl;
+    cout << "4. Terdapat ruam berisi cairan" << endl;
+    cout << "5. Ruam muncul setelah demam turun" << endl;
+    cout << "6. Ruam muncul sdi tangan, kaki, dan mulut" << endl;
+    garis();
+    cout << "DIAGNOSIS:" << endl;
+    cout << "Hand Foot and Mouth Disease (HFMD)" << endl;
+    tindakan1();
+}
+void diagnosis4(){
+    garis();
+    cout << "TANDA DAN GEJALA:" << endl;
+    cout << "1. Terdapat ruam pada kulit." << endl;
+    cout << "2. Tidak terdapat muntah, penurunan kesadaran, dan atau nyeri kepala." << endl;
+    cout << "3. Tidak terdapat ruam berbentuk bintik atau berukuran kecil." << endl;
+    cout << "4. Terdapat ruam berisi cairan" << endl;
+    cout << "5. Ruam muncul setelah demam turun" << endl;
+    cout << "6. Ruam tidak muncul di tangan, kaki, dan mulut" << endl;
+    garis();
+    cout << "DIAGNOSIS:" << endl;
+    cout << "Cacar air atau Varisela" << endl;
+    tindakan1();
+}
+void diagnosis5(){
+    garis();
+    cout << "TANDA DAN GEJALA:" << endl;
+    cout << "1. Terdapat ruam pada kulit." << endl;
+    cout << "2. Tidak terdapat muntah, penurunan kesadaran, dan atau nyeri kepala." << endl;
+    cout << "3. Tidak terdapat ruam berbentuk bintik atau berukuran kecil." << endl;
+    cout << "4. Terdapat ruam berisi cairan." << endl;
+    cout << "5. Ruam tidak muncul setelah demam turun." << endl;
+    cout << "6. Ruam terasa terbakar, dan berada di sekitar mulut atau genital." << endl;
+    garis();
+    cout << "DIAGNOSIS:" << endl;
+    cout << "Herpes Simpleks Virus (HSV)" << endl;
+    tindakan1();
+}
+void diagnosis5(){
+    garis();
+    cout << "TANDA DAN GEJALA:" << endl;
+    cout << "1. Terdapat ruam pada kulit." << endl;
+    cout << "2. Tidak terdapat muntah, penurunan kesadaran, dan atau nyeri kepala." << endl;
+    cout << "3. Tidak terdapat ruam berbentuk bintik atau berukuran kecil." << endl;
+    cout << "4. Terdapat ruam berisi cairan." << endl;
+    cout << "5. Ruam tidak muncul setelah demam turun." << endl;
+    cout << "6. Ruam tidak terasa terbakar, dan tidak berada di sekitar mulut atau genital." << endl;
+    garis();
+    cout << "DIAGNOSIS:" << endl;
+    cout << "Kawasaki Disease" << endl;
+    tindakan3();
+}
